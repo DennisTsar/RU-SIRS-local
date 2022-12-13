9b6ba1e9636790dcf9475474a0b77f118f04cb55
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-12-12 19:16:24 -0500

    Create custom serializer for campuses list

--- src/main/kotlin/data/soc/School.kt
+++ src/main/kotlin/data/soc/School.kt
@@ -1,13 +1,36 @@
 package data.soc
 
 
+import general.Campus
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
-@Serializable
+@Serializable()
 data class School(
-    val campus: String,
+    @SerialName("campus")
+    @Serializable(with = CampusesAsStringSerializer::class)
+    val campuses: List<Campus>, // seems to always be either one or all 3
     val code: String,
     val description: String,
-    val homeCampus: String,
+    val homeCampus: Campus,
     val level: String,
 )
+
+object CampusesAsStringSerializer : KSerializer<List<Campus>> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("CampusesAsStringSerializer", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: List<Campus>) {
+        encoder.encodeString(value.joinToString(","))
+    }
+
+    override fun deserialize(decoder: Decoder): List<Campus> {
+        return decoder.decodeString().split(", ").map { Campus.valueOf(it) }
+    }
+}
